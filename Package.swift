de3dafd5d01f937363faa1853d5993abfcc019a7
tasuwo <kamuhata.you@gmail.com>
2016-04-23 16:44:31 +0900

    Modify name of this bot

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-        name: "kawaiko-chan-slack-bot",
+        name: "KoishiBot",
         dependencies: [
           .Package(url: "https://github.com/tomohisa/SwiftSlackBotter.git", majorVersion: 0, minor: 1),
         ])
